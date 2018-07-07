4c49836d4eff0c2330fc95f727ab14cca12af4d8
Sarga <Sargababu@users.noreply.github.com>
2018-07-06 23:29:29 -0700

    Update test.py
    Second commit

--- test.py
+++ test.py
@@ -25,6 +25,21 @@ class TestClass(object):
         with open('/home/sarga/text_words.txt','r') as f:
             rword = f.read().split(" ")
         return random.choice(rword)
+    
+    def read_lettr(my_word,g_word, trials):    
+        g_lettr = raw_input("Enter your guess: ")
+        out = map(lambda x,y:y if y else "*",my_word, g_word)
+        print ''.join(out)
+        trials = check_word(my_word,g_word,g_lettr,trials)
+        if trials==0 :
+            print"SORRY, YOU LOSE"
+            res = raw_input("Play Again?? type yes or else press any key ")
+            if res.lower() == "yes":
+                game()
+            else:
+                exit(0)
+        read_lettr(my_word,g_word,trials)
+    
 
     def json_to_csv(self, json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
